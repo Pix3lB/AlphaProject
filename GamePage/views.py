2b2f8e6a22c65f436dc2c7f3d3a2d7ad8e66d1fa
Pixel <yusufaffanazmi@gmail.com>
2023-07-19 17:46:51 +0530

    Added Deck and Shuffling

--- GamePage/views.py
+++ GamePage/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
 def GamePage(request):
+    deck=[]
+    DeckBuild(deck)
+    print(deck)
+    random.shuffle(deck)
+    print(deck)
     return render(request, 'GamePage.html')
+
+def DeckBuild(a):
+    values=["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suit=["C", "D", "H", "S"]
+    for i in suit:
+        for j in values:
+            a.append(i+j)
+
