1da4b4f30a2cbacbcfd3278d281104d44b1d554c
Pixel <yusufaffanazmi@gmail.com>
2023-07-21 23:04:02 +0530

    HTML,CSS,Hidden,Value

--- GamePage/views.py
+++ GamePage/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
+Deck=[]
 def GamePage(request):
-    deck=[]
-    DeckBuild(deck)
-    print(deck)
-    random.shuffle(deck)
-    print(deck)
+    DeckBuild(Deck)
+    random.shuffle(Deck)
+    StartGame()
     return render(request, 'GamePage.html')
 
-def DeckBuild(a):
+def DeckBuild(deck):
     values=["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     suit=["C", "D", "H", "S"]
     for i in suit:
         for j in values:
-            a.append(i+j)
-
+            deck.append(i+j)
+def StartGame():
+    Hidden=Deck.pop()
+    DealerValue=+GetValue(Hidden)
+    print("DealerValue\n",DealerValue)
+    print("Hidden\n",Hidden)
+def GetValue(card):
+    if card[-1]=='A':
+        return(11)
+    elif card[-1]=='K' or card[-1]=='J' or card[-1]=='Q':
+        return(10)
+    else:
+        return(int(card[-1]))
